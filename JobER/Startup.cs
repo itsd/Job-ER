524cd32eafa7c3dc862b85d0c627aec52db8484a
tato784 <tato_saxiashvili@mail.ru>
2014-11-04 02:12:32 +0400

    Create Project

---
+++ JobER/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JobER.Startup))]
+namespace JobER
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
