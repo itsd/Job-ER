f905f1ca9f3d3eba8dec2487201949507823fa40
tato784 <tato_saxiashvili@mail.ru>
2014-11-04 02:35:03 +0400

    Continued working on Core

---
+++ JobER.Repositories/UserRepository.cs
@@ -0,0 +1,19 @@
+﻿using JobER.Domain.Interfaces.Repositories;
+using JobER.Repositories.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobER.Repositories {
+    public class UserRepository : IUserRepository {
+        private EntityContext _entityContext;
+
+        public UserRepository(EntityContext entityContext) {
+            _entityContext = entityContext;
+        }
+
+
+    }
+}
